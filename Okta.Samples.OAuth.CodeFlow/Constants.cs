5e57aa5383fcb4657e6a77a473650611c5931ce3
Nate Barbettini <nathanaelb@gmail.com>
2017-12-20 17:02:26 -0800

    Removed old version 🔥

--- Okta.Samples.OAuth.CodeFlow/Constants.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Okta.Samples.OAuth.CodeFlow
-{
-    internal class Constants
-    {
-        internal const string TokenEndpoint = "/oauth2/v1/token";
-        internal const string UserInfoEndpoint = "/oauth2/v1/userinfo";
-    }
-}
--- Okta.Samples.OAuth.CodeFlow/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Okta.Samples.OAuth.CodeFlow.MvcApplication" Language="C#" %>
