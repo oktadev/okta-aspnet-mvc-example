b1e6b5ee4d6bfe9626222509852141186cf7fde0
Nate Barbettini <nathanaelb@gmail.com>
2017-12-22 10:23:12 -0800

    Update bootstrap and add clipboard.js

--- OktaAspNetExample/App_Start/BundleConfig.cs
+++ OktaAspNetExample/App_Start/BundleConfig.cs
@@ -21,7 +21,8 @@ namespace OktaAspNetExample
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js",
-                      "~/Scripts/respond.js"));
+                      "~/Scripts/respond.js",
+                      "~/Scripts/clipboard.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
